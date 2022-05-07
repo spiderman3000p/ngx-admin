2753ee15a27b27a7e2c57a2d1f58bdb6e5447ff1
Rafael Cardona <rafael.cardona@tau-tech.co>
2022-05-06 20:29:33 -0500

    redirect-uri changed to quick-services.azurewebsites.net

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
     clientId: "4becf416-7a56-4bb8-aaec-62ca76a7a4e0", // Application (client) ID from the app registration
     authority:
       "https://login.microsoftonline.com/38cb1598-762b-483f-85a7-a22892d0bbb6", // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-    redirectUri: "https://delightful-meadow-019603e10.1.azurestaticapps.net/", // This is your redirect URI
+    redirectUri: "https://quick-services.azurewebsites.net/", // This is your redirect URI
     profile: "https://graph.microsoft.com/v1.0/me",
   },
   google: {
